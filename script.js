8a679af21f3337b7751e2befedb96cee8c18af39
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-12-04 00:28:29 +0100

    feat: print email in page

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 let email = ''
 let emails = [];
-
+const emailList= document.getElementById('printEmail')
 
 //mi creo una funzione che, richiamando l'endpoint, quando richiamata genera un indirizzo email
+console.log(emails)
+
+generateEmail()
+
+function printList(){
+  emails.forEach(mail => emailList.innerHTML += `<li>${mail}</li>`)
+}
 
 function generateEmail(){
+  
   axios.get(endpoint)
-   .then(response =>{
+  .then(response =>{
     if(response.data.success){
       email = response.data.response
       emails.push(email)    
     }     
+    
+    // genero per 10 volte una email (richiamando l'apposita funzione) e pusho ciascun elemento enerato nell'array 
     if(emails.length < 10){
       generateEmail()
-    } 
+    } else printList()
   })
   .catch(error =>
     console.log(error)
   )
 
- emails
 }
-
-generateEmail()
-console.log(emails)
-// genero per 10 volte una email (richiamando l'apposita funzione) e pusho ciascun elemento enerato nell'array 
-
-   
-
-
 // prendo l'elemento html in cui voglio stampare i miei indirizzi e, in un ciclo for of lo stampo
-
